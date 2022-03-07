69e6d81d325adb59cad3a03ff28cd23e0843c1e5
xomyachok-shaolin <56404900+xomyachok-shaolin@users.noreply.github.com>
2022-03-08 00:59:46 +0500

    add roles & localization

--- backend/Entities/User.cs
+++ backend/Entities/User.cs
@@ -8,6 +8,7 @@ public class User
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Username { get; set; }
+    public List<Role> Roles { get; set; }= new List<Role>();
 
     [JsonIgnore]
     public string PasswordHash { get; set; }
